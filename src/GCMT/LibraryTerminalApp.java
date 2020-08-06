085eb516d8fb9a6e913afe887d539b2c93f0eba4
ben <benchackerman@gmail.com>
2020-08-06 14:04:59 -0400

    saved blueprint

--- src/GCMT/LibraryTerminalApp.java
+++ src/GCMT/LibraryTerminalApp.java
@@ -3,7 +3,17 @@ package GCMT;
 public class LibraryTerminalApp {
 
 	// ~~~ spacers
-	
+	//sysout "are you here to return a book or check out?"
+	//display books in a list up front
+		//books indexed by number
+			//keyword search? then index?
+	//option A have, option B dont have
+		//A - book checks out, book has due date applied
+			//checkout status update, dueDate update
+			//extra--recommend other within genre
+		//B - book not available, display book dueDate for return
+	//after a book is checked out successfully, present updated list with updated checkout status
+	//sysout "here's your book, it's due back XX/XX/XXXX"
 	public static void main(String[] args) {
 		
 	}

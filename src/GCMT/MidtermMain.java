46578271fd3d34a6086790289be57669360f039a
r0flcopt0rz <r0flcopt0rz10611@gmail.com>
2020-08-06 10:48:36 -0400

    try 1

--- src/GCMT/MidtermMain.java
+++ src/GCMT/MidtermMain.java
@@ -3,8 +3,7 @@ package GCMT;
 public class MidtermMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//midterm main method
+	System.out.println("hello all");
 	}
 
 }
